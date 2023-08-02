6cb7d8bc9113fcdd0f350b23ec4649a30443793e
chenhassan1211 <126476031+chenhassan1211@users.noreply.github.com>
2023-08-02 19:26:54 +0300

    Update cc.py

--- cc.py
+++ cc.py
@@ -1,16 +1,24 @@
+import os
+
 def create_and_copy_sentence_to_file():
     # Create the source content
     source_content = "chen is smart"
 
+    # Define the output directory
+    output_dir = "./out/"
+
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+
     # Define the output file name
-    output_file = "output_file.txt"
+    output_file = os.path.join(output_dir, "output_file.txt")
 
     # Create the output file and copy the sentence into it
     with open(output_file, 'w') as file:
         file.write(source_content)
 
     print(f"Sentence '{source_content}' copied to '{output_file}'.")
-    
+
     # Return the output file name
     return output_file
 
@@ -21,4 +29,4 @@ try:
 except Exception as e:
     print(f"An error occurred: {str(e)}")
 
-create_and_copy_sentence_to_file()
+create_and_copy_sentence_to_file
