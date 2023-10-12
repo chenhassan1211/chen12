3d2704411184ef057279dc262ce4bac75317e78a
chenhassan1211 <126476031+chenhassan1211@users.noreply.github.com>
2023-10-12 12:11:17 +0300

    Update hh.py

--- hh.py
+++ hh.py
@@ -1,1 +1,16 @@
+from cnvrgv2 import Cnvrg
+cnvrg = Cnvrg()
+e = Experiment()
+
+loss_vals = []
+# experiment loop:
+for epoch in range(8):
+    loss_vals.append(loss_func())
+
+# attach the chart to the experiment
+loss_chart = LineChart('loss')
+loss_chart.add_series(loss_vals, 's1')
+e.log_metric(loss_chart)
+ 
+
 print("hello")
